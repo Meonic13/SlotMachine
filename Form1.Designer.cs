4ea0dfa3bc35fec9154e4066735f31324d442a51
Meonic <Meonic6996@yandex.ru>
2026-04-23 09:17:49 +0300

    добавлены 3 label для слотов

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,14 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lblSlot1 = new Label();
+            lblSlot2 = new Label();
+            lblSlot3 = new Label();
             SuspendLayout();
+            // 
+            // lblSlot1
+            // 
+            lblSlot1.BackColor = Color.White;
+            lblSlot1.BorderStyle = BorderStyle.FixedSingle;
+            lblSlot1.Font = new Font("Segoe UI Black", 48F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            lblSlot1.Location = new Point(447, 145);
+            lblSlot1.Name = "lblSlot1";
+            lblSlot1.Size = new Size(217, 174);
+            lblSlot1.TabIndex = 0;
+            lblSlot1.Text = "🍒";
+            lblSlot1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblSlot2
+            // 
+            lblSlot2.BackColor = Color.White;
+            lblSlot2.BorderStyle = BorderStyle.FixedSingle;
+            lblSlot2.Font = new Font("Segoe UI Black", 48F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            lblSlot2.Location = new Point(843, 145);
+            lblSlot2.Name = "lblSlot2";
+            lblSlot2.Size = new Size(217, 174);
+            lblSlot2.TabIndex = 1;
+            lblSlot2.Text = "🍒";
+            lblSlot2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblSlot3
+            // 
+            lblSlot3.BackColor = Color.White;
+            lblSlot3.BorderStyle = BorderStyle.FixedSingle;
+            lblSlot3.Font = new Font("Segoe UI Black", 48F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            lblSlot3.Location = new Point(1215, 145);
+            lblSlot3.Name = "lblSlot3";
+            lblSlot3.Size = new Size(217, 174);
+            lblSlot3.TabIndex = 2;
+            lblSlot3.Text = "🍒";
+            lblSlot3.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkGreen;
-            ClientSize = new Size(474, 379);
+            ClientSize = new Size(2206, 910);
+            Controls.Add(lblSlot3);
+            Controls.Add(lblSlot2);
+            Controls.Add(lblSlot1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             Name = "MainForm";
@@ -45,5 +87,9 @@
         }
 
         #endregion
+
+        private Label lblSlot1;
+        private Label lblSlot2;
+        private Label lblSlot3;
     }
 }
